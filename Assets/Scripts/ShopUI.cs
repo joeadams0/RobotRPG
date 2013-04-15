8058c72983f663a37fe8b9b61f553dc2aef789df
JSatterf1579 <joseph.satterfield@case.edu>
2013-04-15 12:22:27 -0400

    Started work on shop UI, organized files into folders
    Signed-off-by: JSatterf1579 <joseph.satterfield@case.edu>

---
+++ Assets/Scripts/ShopUI.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class ShopUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	///<description>
+	///The basic building block used in the shop's interface
+	///Contains a picture, a name, a short description for the item, the price, and a buy button 
+	///</description>
+	bool shopBlock ()
+	{
+		return true;
+	}
+}
